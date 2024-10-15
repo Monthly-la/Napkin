053e0679342c38e02ae79220e8857248049ed9cc
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-15 01:54:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -249,7 +249,7 @@ def load_navbar(user_name):
     <div class="navbar">
         <div class="search-container">
             <input type="text" placeholder="Search">
-            <button type="submit">&#xFE0E</button>
+            <button type="submit">🔎︎</button>
         </div>
         <div class="dropdown">
             <button class="dropbtn">{user_name} &#9662;</button>
