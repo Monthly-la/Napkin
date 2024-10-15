4e917d6fc572dc075cbd7190447984dac8d1d9dc
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-15 01:16:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ def process_files(uploaded_files):
 
 
 # Streamlit app
-col1, colA, col2, colB, col3 = st.columns([8,1,6,1,6])
+col1, colA, col2, colB, col3 = st.columns([12,1,9,1,9])
 with col1:
     st.title('Credit Card Statement Processor')
     
