d3e44a129c12fd63d2d9bf33bd876908ca7f72b2
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-15 16:27:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,6 +320,7 @@ with tab2:
         col2, colB, col3 = st.columns([11,1,11])
         st.markdown("")
         with col2:
+            st.markdown("Movimientos de Saldo (MXN)")
             if 'data' in st.session_state and not st.session_state.data.empty:
                 dates_js = edited_data['Fecha'].dt.strftime('%Y-%m-%d').tolist()  # Format dates as strings
                 values_js = edited_data['Monto Acumulado'].tolist()
@@ -367,6 +368,7 @@ with tab2:
                 html(chart_code, height=500)
     
         with col3:
+            st.markdown("Egreso total por Comercio (MXN)")
             if 'data' in st.session_state and not st.session_state.data.empty:
                 df_summary = edited_data[['Comercio', 'Monto']].groupby('Comercio').sum()
                 df_sorted = df_summary.sort_values('Monto', ascending=True).reset_index()
