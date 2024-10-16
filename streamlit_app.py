83c58655577cb91372117a7445e11ca98ceb9f33
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-15 22:52:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,15 @@ hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
+footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
 .css-hi6a2p {padding-top: 0rem;}
 .stDeployButton {visibility: hidden;}
 </style>
