ff1e5bbac6b563f217cf1df28d8a850632999aab
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-15 18:43:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -326,7 +326,7 @@ with tab1:
         st.markdown("")
         # Display and edit data using session state
         if 'data' in st.session_state and not st.session_state.data.empty:
-            edited_data = st.data_editor(st.session_state.data, num_rows="dynamic")
+            edited_data = st.data_editor(st.session_state.data, num_rows="dynamic", hide_index=True)
         
             if st.button('Generate Graphs'):
                 graphs = True
